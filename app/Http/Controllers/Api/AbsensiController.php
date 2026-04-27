2d160754186cedd09a3080d66ab6fa81441e0089
Deni Tri Muslimin <denitrimuslimin@gmail.com>
2026-04-27 15:42:31 +0700

    first commit

--- app/Http/Controllers/Api/AbsensiController.php
+++ app/Http/Controllers/Api/AbsensiController.php
@@ -176,15 +176,23 @@ class AbsensiController extends Controller
         'message' => 'Bukan jam absen'
     ], 200);
 }
-    public function today(Request $request)
-    {
-        $now = Carbon::now();
-        $tahun = Periode::where('tahun',$now->year)->first();
-        $data = Absensi::where('pegawai_nip', $request->employee_id)
-            ->where('tgl', $now->day)
-            ->where('bulan', $now->month)
-            ->where('periode_id', $tahun->id)
-            ->first();
+
+public function today(Request $request)
+{
+    $now = Carbon::now();
+    $tahun = Periode::where('tahun', $now->year)->first();
+    if (!$tahun) {
+        return response()->json([
+            'status' => false,
+            'message' => 'Periode belum tersedia',
+            'data' => null
+        ]);
+    }
+    $data = Absensi::where('pegawai_nip', $request->employee_id)
+        ->where('tgl', $now->day)
+        ->where('bulan', $now->month)
+        ->where('periode_id', $tahun->id)
+        ->first();
 
         return response()->json([
             'status' => true,
@@ -193,129 +201,20 @@ class AbsensiController extends Controller
     }
 }
 
-// public function scan(Request $request)
-// {
-//     $request->validate([
-//         'employee_id' => 'required',
-//         'qr_code' => 'required'
-//     ]);
-//     $now = Carbon::now();
-//     // VALIDASI QR
-//     $data = json_decode($request->qr_code, true);
-//     if (!$data || !isset($data['type'])) {
+//     public function today(Request $request)
+//     {
+//         $now = Carbon::now();
+//         $tahun = Periode::where('tahun',$now->year)->first();
+//         $data = Absensi::where('pegawai_nip', $request->employee_id)
+//             ->where('tgl', $now->day)
+//             ->where('bulan', $now->month)
+//             ->where('periode_id', $tahun->id)
+//             ->first();
+//    $data = $data ?? [];
 //         return response()->json([
-//             'message' => 'QR tidak valid'
-//         ], 400);
-//     }
-//     if ($data['type'] !== 'absen') {
-//         return response()->json([
-//             'message' => 'QR tidak sesuai'
-//         ], 400);
-//     }
-//     // cek SECRET
-//     if (!isset($data['key']) || $data['key'] !== env('QR_SECRET')) {
-//         return response()->json([
-//             'message' => 'QR ilegal'
-//         ], 403);
-//     }
-//     // cek periode (bulanan)
-//     if (!isset($data['periode']) || $data['periode'] !== $now->format('Y-m')) {
-//         return response()->json([
-//             'message' => 'QR bukan periode ini'
-//         ], 400);
-//     }
-//     // cek expired
-//     if (isset($data['expired']) && $now->toDateString() > $data['expired']) {
-//         return response()->json([
-//             'message' => 'QR sudah kadaluarsa'
-//         ], 400);
-//     }
-//     // CEK PERIODE DB
-//     $periode = Periode::where('bulan', $now->month)
-//         ->where('tahun', $now->year)
-//         ->first();
-//     if (!$periode) {
-//         return response()->json([
-//             'message' => 'Periode tidak ditemukan'
-//         ], 404);
-//     }
-//     // CEK PEGAWAI
-//     $pegawai = Pegawai::where('nip', $request->employee_id)->first();
-//     if (!$pegawai) {
-//         return response()->json([
-//             'message' => 'Pegawai tidak ditemukan'
-//         ], 404);
-//     }
-//     // JAM ABSEN
-//     $isMorning = $now->hour >= 6 && $now->hour <= 9;
-//     $isAfternoon = $now->hour >= 16 && $now->hour <= 19;
-//     $sudahAbsen = Absensi::where('pegawai_nip', $pegawai->nip)
-//         ->where('tgl', $now->day)
-//         ->where('periode_id', $periode->id)
-//         ->where('bulan', $periode->bulan)
-//         ->first();
-//     // ABSEN PAGI
-//     if ($isMorning) {
-//         if (!$sudahAbsen) {
-//             $absen = Absensi::create([
-//                 'pegawai_nip' => $pegawai->nip,
-//                 'periode_id' => $periode->id,
-//                 'bulan' => $periode->bulan,
-//                 'tgl' => $now->day,
-//                 'jam_masuk_pagi' => $now->format('H:i:s'),
-//                 'pagi' => 1
-//             ]);
-//         } else {
-//             if ($sudahAbsen->pagi == 1) {
-//                 return response()->json([
-//                     'message' => 'Sudah absen pagi'
-//                 ], 409);
-//             }
-//             $sudahAbsen->update([
-//                 'jam_masuk_pagi' => $now->format('H:i:s'),
-//                 'pagi' => 1
-//             ]);
-//             $absen = $sudahAbsen;
-//         }
-//     }
-//     // ABSEN SORE
-//     elseif ($isAfternoon) {
-//         if (!$sudahAbsen) {
-//             // otomatis buat record walau belum pagi
-//                 $absen = Absensi::create([
-//                     'pegawai_nip' => $pegawai->nip,
-//                     'periode_id' => $periode->id,
-//                       'bulan' => $periode->bulan,
-//                     'tgl' => $now->day,
-//                     'jam_masuk_sore' => $now->format('H:i:s'),
-//                     'sore' => 1
-//                 ]);
-//                 return response()->json([
-//                     'message' => 'Absen sore (tanpa absen pagi)',
-//                     'data' => $absen
-//                 ]);
-//         }
-//         if ($sudahAbsen->sore == 1) {
-//             return response()->json([
-//                 'message' => 'Sudah absen sore'
-//             ], 409);
-//         }
-//         $sudahAbsen->update([
-//             'jam_masuk_sore' => $now->format('H:i:s'),
-//             'sore' => 1
+//             'status' => true,
+//             'data' => $data
 //         ]);
-//         $absen = $sudahAbsen;
-//     }
-//     // BUKAN JAM ABSEN
-//     else {
-//         return response()->json([
-//             'message' => 'Bukan jam absen'
-//         ], 422);
-//     }
-//     return response()->json([
-//         'message' => 'Absen berhasil',
-//         'data' => $absen
-//     ]);
 //     }
 
 
